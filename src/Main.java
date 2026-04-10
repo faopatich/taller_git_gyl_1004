9989a3532e464f8dbff768a0dcaecbc4f92a2091
faopatich <francoopatich@gmail.com>
2026-04-10 13:24:36 -0300

    Carga de programa de calculadora

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,18 @@
-void main() {
-    
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Ingrese un numero: ");
+        Float Num1 = teclado.nextFloat();
+        System.out.print("Ingrese un numero: ");
+        Float Num2 = teclado.nextFloat();
+
+        System.out.println(Sumar(Num1, Num2));
+
+    }
+
+    public static float Sumar(float Num1, float Num2){
+        return Num1 + Num2;
+    }
 }
